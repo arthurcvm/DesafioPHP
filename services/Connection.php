8e1bb8aa06c4d51f77d49b5c2dee6394a812fa81
Arthur Cezar <arthurcvm@hotmail.com>
2021-02-02 20:44:18 -0300

    Configuração de conexão com o banco de dados

---
+++ services/Connection.php
@@ -0,0 +1,14 @@
+<?php
+class Connection
+{
+    private static $instance;
+
+    public static function getConn()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new PDO('pgsql:host=localhost;port=54320;dbname=desafio;user=homestead;password=secret');
+        } else {
+            return self::$instance;
+        }
+    }
+}
