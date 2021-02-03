187bb889d8cdf07168b5af74c4b90615caf96e64
Arthur Cezar <arthurcvm@hotmail.com>
2021-02-02 21:57:04 -0300

    Estrutura da model de ProductType

---
+++ app/model/DAO/ProductTypeDao.php
@@ -0,0 +1,21 @@
+<?
+
+namespace app\model\DAO;
+
+class ProductTypeDao {
+    public function create(ProductType $pt){
+
+    }
+
+    public function read() {
+
+    }
+
+    public function update(ProductType $pt){
+
+    }
+
+    public function delete($id){
+
+    }
+}
