ec97dae881e0b4a10cfae3faacc640ec4c17e3e3
Arthur Cezar <arthurcvm@hotmail.com>
2021-02-03 21:17:12 -0300

    Construção de Dao de sales

--- App/Model/SaleDao.php
+++ App/Model/SaleDao.php
@@ -2,26 +2,52 @@
 
 namespace App\Model;
 
+use PDO;
 
-class SaleDao {
-    public function create(Sale $s){
-        $sql = 'INSERT INTO sales(id, date_sale) VALUES (?, ?)';
+class SaleDao
+{
+    public function create(Sale $s)
+    {
+        $sql = 'INSERT INTO sales(date_sale) VALUES (?)';
 
         $stmt = Connection::getConn()->prepare($sql);
-        $stmt->bindValue(1, $s->getId());
-        $stmt->bindValue(2, $s->getDateSale());
+        $stmt->bindValue(1, $s->getDateSale());
         $stmt->execute();
     }
 
-    public function read() {
+    public function read()
+    {
+        $sql = 'SELECT * FROM sales';
 
+        $stmt = Connection::getConn()->prepare($sql);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $res;
+        }
+        return [];
     }
 
-    public function update(Sale $s){
+    public function update(Sale $s)
+    {
 
+        $sql = 'UPDATE sales SET date_sale = ? WHERE id = ?';
+
+        $stmt = Connection::getConn()->prepare($sql);
+        $stmt->bindValue(1, $s->getDateSale());
+        $stmt->bindValue(2, $s->getId());
+
+        $stmt->execute();
     }
 
-    public function delete($id){
+    public function delete($id)
+    {
+        $sql = 'DELETE FROM sales WHERE id = ?';
 
+        $stmt = Connection::getConn()->prepare($sql);
+        $stmt->bindValue(1, $id);
+
+        $stmt->execute();
     }
 }
